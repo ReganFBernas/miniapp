219f3120d720d9ab90a404619139e5f848ee4c77
ReganFBernas <regan.bernas@gmail.com>
2020-02-19 23:09:05 +0800

    new file

---
+++ resources/views/song.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="1">
+<head>
+
+</head>
+
+<body>
+<h1>LIST OF ALL SONG</h1>
+<table border="1">
+  <thead>
+    <th>ID</th>
+    <th>Title of the Song</th>
+    <th>Artist</th>
+  </thead>
+  
+</body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Online Canteen</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    UNC CANTEEN
                 </div>
 
                 <div class="links">
